78792dd25c28e706d79b1e9b59cb4b3b73b5a851
usha <52576706+Usha-29@users.noreply.github.com>
2020-06-03 14:25:37 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,3 +1,11 @@
-<script>
-  console.log("Hi");
-</script>
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
